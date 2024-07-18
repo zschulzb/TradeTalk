8cc869e7e885e132f4f744f5f48f517df4b4c89c
zschulzb <zacharyksb@gmail.com>
2024-07-18 07:07:24 -0500

    mv app.py to app/route.py

---
+++ app/routes.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+def landpage():
+    return render_template("home.html")
+
+@app.route("/Sign_In")
+def signin():
+    return render_template("signin.html")
+
+@app.route("/About_Us")
+def aboutus():
+    return render_template("aboutus.html")
+
+@app.route("/Info")
+def info():
+    return render_template("info.html")
+
+
+#done routing blocks
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # the combination of host and port are where we look for it in our browser
+    # e.g. http://0.0.0.0:8080/
+    # setting debug=True to see error messages - wouldn't do this on a final deployed app
