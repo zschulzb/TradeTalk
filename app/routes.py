c8d126908e603e654b62fbed6c2d1a9ee5195f7b
zschulzb <zacharyksb@gmail.com>
2024-08-08 19:17:24 -0500

    add url_for lib

--- app/routes.py
+++ app/routes.py
@@ -38,7 +38,7 @@ def signin():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={})'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template("signin.html", user=user, post=post, form=form)
 
 @app.route("/About_Us")
